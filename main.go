7788f4188700b7219efd18e25e98b57ea6645048
esarizki15 <esarizki15@gmail.com>
2026-01-25 07:32:22 +0700

    add categories api

--- main.go
+++ main.go
@@ -20,6 +20,84 @@ var produk = []Produk{
 	{ID: 2, Nama: "Indomie Sawit", Harga: 2000, Stok: 8},
 }
 
+type Category struct {
+	ID          int    `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+}
+
+var categories = []Category{
+	{ID: 1, Name: "Makanan", Description: "Makanan ringan dan berat"},
+	{ID: 2, Name: "Minuman", Description: "Minuman segar"},
+}
+var lastCategoryID = 2
+
+// --- HANDLER CATEGORY ---
+// Handle GET All & POST
+func handleCategories(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	if r.Method == "GET" {
+		json.NewEncoder(w).Encode(categories)
+	} else if r.Method == "POST" {
+		var newCat Category
+		if err := json.NewDecoder(r.Body).Decode(&newCat); err != nil {
+			http.Error(w, "Invalid body", http.StatusBadRequest)
+			return
+		}
+		lastCategoryID++
+		newCat.ID = lastCategoryID
+		categories = append(categories, newCat)
+		w.WriteHeader(http.StatusCreated)
+		json.NewEncoder(w).Encode(newCat)
+	} else {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+}
+
+// Handle GET Detail, PUT, DELETE
+func handleCategoryByID(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	idStr := strings.TrimPrefix(r.URL.Path, "/api/categories/")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "Invalid Category ID", http.StatusBadRequest)
+		return
+	}
+
+	// Cari index kategori
+	index := -1
+	for i, c := range categories {
+		if c.ID == id {
+			index = i
+			break
+		}
+	}
+
+	if index == -1 {
+		http.Error(w, "Category not found", http.StatusNotFound)
+		return
+	}
+
+	if r.Method == "GET" {
+		json.NewEncoder(w).Encode(categories[index])
+	} else if r.Method == "PUT" {
+		var updateCat Category
+		if err := json.NewDecoder(r.Body).Decode(&updateCat); err != nil {
+			http.Error(w, "Invalid body", http.StatusBadRequest)
+			return
+		}
+		updateCat.ID = id
+		categories[index] = updateCat
+		json.NewEncoder(w).Encode(updateCat)
+	} else if r.Method == "DELETE" {
+		categories = append(categories[:index], categories[index+1:]...)
+		json.NewEncoder(w).Encode(map[string]string{"message": "Category deleted"})
+	} else {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+}
+
 func getProdukByID(w http.ResponseWriter, r *http.Request) {
 	idStr := strings.TrimPrefix(r.URL.Path, "/api/produk/")
 	id, err := strconv.Atoi(idStr)
@@ -143,6 +221,9 @@ func main() {
 			"message": "Api Running",
 		})
 	})
+
+	http.HandleFunc("/api/categories", handleCategories)
+	http.HandleFunc("/api/categories/", handleCategoryByID)
 
 	fmt.Println("Server running di localhost:8080")
 	err := http.ListenAndServe(":8080", nil)
